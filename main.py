79e49c94a61fdd3d1fbab84a21d9c949ed5e3c88
LabServices <labservices@MEM3245-0231.local>
2022-07-28 09:39:26 -0400

    Initial

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-# Background
-background = pygame.image.load('background.jpg')
-
-
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
@@ -43,15 +39,6 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
-
-
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    screen.blit(bulletImg, (x + 10, y - 10))
-
 
 # Game Loop
 running = True
@@ -59,8 +46,6 @@ while running:
 
     # RGB - Red, Green, Blue
     screen.fill((0, 0, 0))
-    # Background image
-    screen.blit(background, ((0, 0)))
 
     # Catch pygames QUIT event and exit the while loop
     for event in pygame.event.get():
@@ -73,10 +58,7 @@ while running:
                 playerX_change -= 0.3
             elif event.key == pygame.K_RIGHT:
                 playerX_change += 0.3
-            if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
-                    bulletX = playerX
-                    fire_bullet(bulletX, bulletY)
+
 
         # if key is released
         if event.type == pygame.KEYUP:
@@ -85,32 +67,6 @@ while running:
 
     playerX += playerX_change
 
-    # Player Boundry logic
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 752:
-        playerX = 752
-
-    # Enemy Boundry logic
-    enemyX += enemyX_change
-
-    if enemyX <= 0:
-        enemyX_change = 0.3
-        enemyY += enemyY_change
-    elif enemyX >= 752:
-        enemyX_change = -0.3
-        enemyY += enemyY_change
-
-    # Bullet Movement
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
-
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
-
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
     # Update screen
     pygame.display.update()
